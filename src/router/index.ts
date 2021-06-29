f2ced6aaa64ad48d48795b5bf4a210d3bee46c71
dowar <dowar@disroot.org>
2021-06-29 11:11:43 -0600

    version movil

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,28 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
+import Inicio from "../views/Inicio.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/home'
+    path: "/",
+    redirect: "/inicio",
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  }
-]
+    path: "/home",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/inicio",
+    name: "Inicio",
+    component: Inicio,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Inicio.vue
@@ -0,0 +1,340 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Inicio</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-row>
+        <ion-col size="12" class="center">
+          <img
+            class="logo-utp"
+            src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.utparral.edu.mx%2Flogotipos%2Flogo%2520utp%2520trazos%2520fondos%2520obscuros.png&f=1&nofb=1"
+            alt=""
+            srcset=""
+          />
+        </ion-col>
+      </ion-row>
+
+      <ion-row>
+        <ion-col size="12">
+          <h1 style="text-align: center;">
+            Redes Inteligentes y Ciberseguridad
+          </h1>
+        </ion-col>
+      </ion-row>
+
+      <ion-row>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>Competencias Profesionales</ion-card-title>
+              <ion-card-content>
+                Las competencias profesionales son las destrezas y actitudes que
+                permiten al Ingeniero desarrollar actividades en su área
+                profesional, adaptarse a nuevas situaciones, así como
+                transferir, si es necesario, sus conocimientos, habilidades y
+                actitudes a áreas profesionales próximas.
+              </ion-card-content>
+            </ion-card-header>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>Competencias Genéricas</ion-card-title>
+              <ion-card-content>
+                <ion-list>
+                  <ion-item>
+                    <p>
+                      Plantear y solucionar problemas con base en los principios
+                      y teorías de física, química y matemáticas, a través del
+                      método científico para sustentar la toma de decisiones en
+                      los ámbitos científico y tecnológico.
+                    </p>
+                  </ion-item>
+                  <ion-item>
+                    <p>
+                      Desarrollar y fortalecer las habilidades instrumentales,
+                      interpersonales, sistémicas y directivas para comunicarse
+                      en un segundo idioma.
+                    </p>
+                  </ion-item>
+                </ion-list>
+              </ion-card-content>
+            </ion-card-header>
+          </ion-card>
+        </ion-col>
+      </ion-row>
+
+      <ion-row class="ion-padding">
+        <ion-col size="12">
+          <h2>Competencias específicas</h2>
+        </ion-col>
+        <ion-col size="12">
+          <p>
+            Dirigir proyectos de tecnologías de información, para contribuir a
+            la productividad y logro de los objetivos estratégicos de las
+            organizaciones utilizando las metodologías apropiadas.
+          </p>
+        </ion-col>
+      </ion-row>
+
+      <ion-row>
+        <ion-col size="12" class="ion-padding">
+          <h3>
+            Escenarios de Actuación
+          </h3>
+        </ion-col>
+        <ion-col size="12" class="ion-padding">
+          <h4>
+            El Ingeniero en Redes Inteligentes y Ciberseguridad podrá
+            desenvolverse en:
+          </h4>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Empresas públicas y privadas dedicadas al desarrollo y consultoría
+              de software.
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Empresas dedicadas a la venta e integración de servicios
+              tecnológicos en infraestructura física y lógica.
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Empresas privadas de la industria de TI, enfocadas en el
+              desarrollo de software y automatización de infraestructura
+              informática.
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              En cualquier empresa que requiera de un la implementación y
+              administración de servicios de red para eficiente sus procesos a
+              partir de una base tecnológica.
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Empresas transnacionales dedicadas a la venta se servicios
+              administrados en la nube.
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Empresas públicas y privadas dedicadas al desarrollo y consultoría
+              de software.
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Empresas extranjeras dedicadas a la consultoría e implementación
+              de proyectos de seguridad informática.
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              En empresas públicas y privadas que manejen sistemas y
+              comunicaciones digitales para eficientar sus procesos.
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Empresas públicas y privadas en proceso de migración, evolución y
+              crecimiento de su infraestructura tecnológico.
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Empresas emergentes dedicadas a la innovación y el desarrollo
+              tecnológico.
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              En organizaciones públicas y privadas que cuenten con estrategias
+              de protección y seguridad de información.
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              De forma independiente mediante consultorías de bienes y
+              servicios.
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+      </ion-row>
+
+      <ion-row>
+        <ion-col size="12" class="ion-padding">
+          <h3>
+            Ocupaciones Profesionales
+          </h3>
+        </ion-col>
+        <ion-col size="12" class="ion-padding">
+          <h4>
+            El Ingeniero en Redes Inteligentes y Ciberseguridad podrá
+            desenvolverse en:
+          </h4>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Site Reliability Engineering
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              ngeniero de cómputo en la nube
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              DevOps Engineer
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Especialista en ciberseguridad
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Data Protection Officer
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Integrador de soluciones de infraestructura de redes inteligentes
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Administrador de Sistemas
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Auditor de TI
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Líder de proyectos de infraestructura de redes inteligentes y
+              ciberseguridad
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Administrador de redes
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col size="12">
+          <ion-card>
+            <ion-card-content>
+              Analista de Ciberseguridad
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+      </ion-row>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonSegment,
+  IonSegmentButton,
+  IonItem,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Inicio",
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+    IonItem,
+  },
+  data() {
+    return {
+      segment: "escenariosActuacion",
+    };
+  },
+});
+</script>
+
+<style scoped>
+.logo-utp {
+  width: 12rem;
+}
+.center {
+  display: flex;
+  justify-content: center;
+}
+</style>
